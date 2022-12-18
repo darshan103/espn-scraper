ef264bdeaf348dd6862849fb1fdec5fe55c42f36
Darshan Singh <singhdarshan2166@gmail.com>
2022-12-18 11:51:35 +0530

    second commit

--- main.js
+++ main.js
@@ -18,5 +18,29 @@ function extractLink(html){
     let anchorEle = $('.ds-border-t.ds-border-line.ds-text-center.ds-py-2 a');
     let link = anchorEle.attr('href');
     let fullLink = 'https://www.espncricinfo.com'+link;
-    console.log(fullLink);
+    // console.log(fullLink);
+    getAllMatchLink(fullLink);
+}
+
+function getAllMatchLink(uri){
+    request(uri, function(error, response, html){
+        if(error){
+            console.log(error);
+        }
+        else{
+            extractAllMatchLink(html)
+        }
+    })
+}
+
+function extractAllMatchLink(html){
+    let $ = cheerio.load(html);
+    let scoreArr = $('.ds-grow.ds-px-4.ds-border-r.ds-border-line-default-translucent .ds-no-tap-higlight');
+    let length = scoreArr.length;
+    console.log(length);
+    for(let i=0; i<scoreArr.length; i=i+6){
+        let scoreLink = $(scoreArr[i]).attr('href');
+        let fullLink = 'https://www.espncricinfo.com'+scoreLink;
+        console.log(fullLink);
+    }
 }
