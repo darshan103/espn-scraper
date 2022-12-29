4c8028820ce38dbed87df9ced28f410afdcc3c4c
Darshan Singh <singhdarshan2166@gmail.com>
2022-12-29 13:56:35 +0530

    fourth commit

--- scoreCard.js
+++ scoreCard.js
@@ -37,7 +37,36 @@ function extractMatchDetails(html){
         // console.log(teamName);
         // console.log(oppName);
 
-        console.log(venue, date, result, teamName, oppName);
+        let cInnings = $(innings[i]);
+        let allRows = cInnings.find('table.ds-w-full.ds-table.ds-table-md.ds-table-auto.ci-scorecard-table tbody tr');
+        // let count=allRows.length;
+        // console.log(count);
+        for(let j=0; j<=allRows.length; j++){
+            let allCols =$(allRows[j]).find('td');
+
+            // I am using counting for fetching players details.
+            let count = allCols.length;
+            // console.log(count);
+
+            // let isWorthy=$(allCols[0]).hasClass('batsmen-table');
+            // if(isWorthy==true){
+            //     let playerName = $(allCols[0]).text().trim();
+            //     console.log(playerName);
+            // }
+
+            if(count==8){
+                let playerName = $(allCols[0]).text().trim();
+                let runs = $(allCols[2]).text().trim();
+                let balls = $(allCols[3]).text().trim();
+                let fours = $(allCols[5]).text().trim();
+                let sixs = $(allCols[6]).text().trim();
+                let str = $(allCols[7]).text().trim();
+                console.log(playerName, runs, balls, fours, sixs, str);
+            }
+        }
+        console.log("...................Opponent_Team......................");
+
+        // console.log(venue, date, result, teamName, oppName);
 
     }
 }
